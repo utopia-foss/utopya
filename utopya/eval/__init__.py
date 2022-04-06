0880ad5657d1a2d30a485c28ab94d0ae4b4ba10c
Yunus Sevinchan <yunussevinchan@gmail.com>
2022-04-06 15:46:09 +0200

    Make more names available from utopya.eval module

--- utopya/eval/__init__.py
+++ utopya/eval/__init__.py
@@ -2,7 +2,10 @@
 
 from dantro.plot_creators import is_plot_func
 
+from .containers import GridDC, NumpyDC, XarrayDC, XarrayYamlDC
 from .datamanager import DataManager
 from .groups import MultiverseGroup, UniverseGroup
+from .plotcreators import MultiversePlotCreator, UniversePlotCreator
+from .plothelper import PlotHelper
 from .plotmanager import PlotManager
 from .transform import register_operation
