8ec0db280e5181421179a785b6421a719ae92037
Yunus Sevinchan <yunussevinchan@gmail.com>
2022-06-13 11:57:04 +0200

    Fix and expand eval tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -17,7 +17,7 @@ logging.getLogger("utopya.reporter").setLevel(logging.INFO)
 DUMMY_MODEL = "MinimalModel"
 """Dummy model to use for testing basic functionality"""
 
-ADVANCED_MODEL = "MinimalModel"  # FIXME
+ADVANCED_MODEL = "ExtendedModel"
 """Model to use for testing advanced functionality"""
 
 DEMO_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), "../demo"))
