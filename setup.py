05feedcad64f709f355e89f46c1b03eb70269ddc
Thomas Gaskin <tgaskin@live.co.uk>
2022-07-18 17:14:50 +0000

    Use new custom dantro scatter plot

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ INSTALL_DEPS = [
     #
     # related to utopya:
     "paramspace>=2.5.9",
-    "dantro>=0.18.0a11",
+    "dantro>=0.18.0b1",
 ]
 # NOTE When adding a new dependency, make sure to denote it in the isort
 #      configuration, see pyproject.toml.
