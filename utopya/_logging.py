82ad3c1b6ff9dac9479a3cd3fd75b6681533c99b
Yunus Sevinchan <blsqr@posteo.com>
2025-10-27 14:03:01 +0100

    Fix utopya logging showing `logging` instead of module name
    Need a custom `shortname` filter for that.

--- utopya/_logging.py
+++ utopya/_logging.py
@@ -1,30 +1,46 @@
 """Sets up logging, based on dantro's logging features"""
 
-import os
+import logging
 
 import coloredlogs as _coloredlogs
 from dantro.logging import REMARK as _DEFAULT_LOG_LEVEL
 from dantro.logging import getLogger as _getLogger
 
+# -----------------------------------------------------------------------------
+
+
+class ShortNameFilter(logging.Filter):
+    """A logging filter that adds the ``shortname`` attribute with just the
+    module name to the logging record.
+
+    This allows the log format to show, for example, 'multiverse' instead of
+    'utopya.multiverse'.
+    """
+
+    def filter(self, record):
+        record.shortname = (
+            record.name.split(".")[-1] if "." in record.name else record.name
+        )
+        return True
+
+
+# -- Logger Setup -------------------------------------------------------------
+
 _log = _getLogger("utopya")
 """The utopya root logger"""
-
-_DEFAULT_LOG_FORMAT = "%(levelname)-8s %(module)-16s  %(message)s"
-"""The default logging format"""
 
 # Add colour logging to the root logger
 # See API reference:  https://coloredlogs.readthedocs.io/en/latest/api.html
 _coloredlogs.install(
-    logger=_log,
     level=_DEFAULT_LOG_LEVEL,
-    fmt=_DEFAULT_LOG_FORMAT,
+    fmt="%(levelname)-8s %(shortname)-16s  %(message)s",
     level_styles=dict(
         trace=dict(faint=True),
         debug=dict(faint=True),
         remark=dict(color=246),  # grey
         note=dict(color="cyan"),
         info=dict(bright=True),
-        ping=dict(color="yellow"),
+        ping=dict(color="yellow", bright=True),
         progress=dict(color="green"),
         caution=dict(color=202),  # orange
         hilight=dict(color="yellow", bold=True),
@@ -34,7 +50,17 @@ _coloredlogs.install(
         critical=dict(color="red", bold=True),
     ),
     field_styles=dict(
-        levelname=dict(bold=True, faint=True), module=dict(faint=True)
+        levelname=dict(bold=True, faint=True),
+        module=dict(faint=True),
+        name=dict(faint=True),
+        shortname=dict(faint=True),
     ),
 )
+
+# Also install the filter on (existing) handlers; need to do this as some
+# handlers (of other modules) are already set up and may thus lack the filter.
+_shortname_filter = ShortNameFilter()
+for handler in logging.root.handlers:
+    handler.addFilter(_shortname_filter)
+
 _log.debug("Logging configured.")
